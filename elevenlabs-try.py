5ec5fa733e54eb599aec6f92d9406ec615a24c26
Amir Kidwai <40836125+cumbersomeamir@users.noreply.github.com>
2023-05-08 10:46:26 +0100

    Update elevenlabs-try.py

--- elevenlabs-try.py
+++ elevenlabs-try.py
@@ -1,6 +1,7 @@
 #pip3 install elevenlabs
 
-from elevenlabs import generate, play, set_api_key
+import os
+from elevenlabs import generate, set_api_key
 
 # set_api_key("<YOUR_API_KEY>") # Check the guide on how to get a free API key: https://docs.elevenlabs.io/authentication/01-xi-api-key
 
@@ -9,5 +10,11 @@ audio = generate(
     voice="Bella"
 )
 
-play(audio, notebook=True)
+# Save the audio to a file
+with open("output.wav", "wb") as f:
+    f.write(audio)
+
+# Play the audio file using a command-line tool (assuming the audio format is WAV)
+os.system("aplay output.wav")
+
 
